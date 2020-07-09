3b72fe57d33eeeb048d0eddfd1e56c60e95e52a8
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-07-10 00:10:58 +0530

    Load items from vector

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+extern crate gio;
+extern crate gtk;
+
+pub mod ui {
+    use super::config;
+    use gio::prelude::*;
+    use gtk::prelude::*;
+    use gtk::{Application, ApplicationWindow, Builder, Button, ComboBoxText, ComboBoxTextExt};
+
+    pub fn app_ui() {
+        let main_window_path = "main.glade";
+
+        if gtk::init().is_err() {
+            println!("Failed to initialize GTK.");
+            return;
+        }
+        let builder = gtk::Builder::from_file(main_window_path);
+        let window: gtk::Window = builder.get_object("main_window").unwrap();
+        let choice_picker: gtk::ComboBoxText = builder.get_object("info_choices").unwrap();
+        let categories = config::get_categories();
+        for item in categories.iter() {
+            ComboBoxTextExt::append(&choice_picker, Some("info_choices"), item);
+        }
+        window.show_all();
+        gtk::main();
+    }
+}
+
+pub mod config {
+    pub fn get_categories() -> std::vec::Vec<&'static str> {
+        return vec!["CPU", "Hardware", "Network", "PCI", "SATA/SCSI", "Misc."];
+    }
+}
